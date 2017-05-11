7c1d3c0f9cf8cfc599a8e2ba6c6f0b37a3171439
ThaoTV <vietnamthaotranvan@gmail.com>
2017-05-12 01:08:20 +0700

    generate module and component

--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ProductComponent } from './product/product.component';
+import { RoleComponent } from './role/role.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [ProductComponent, RoleComponent]
 })
 export class MainModule { }
